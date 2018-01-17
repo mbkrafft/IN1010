ecf75867a8f89cc144b3fa5d44cb177dd8f36c71
Mathias Krafft <mbkrafft@gmail.com>
2018-01-17 16:46:33 +0100

    Flere trixoppgaver

---
+++ trix/1.15/Test.java
@@ -0,0 +1,13 @@
+
+class Test {
+
+    public static void main(String[] args) {
+
+        int jens;
+        int siv = Jens * 3;
+
+        System.out.println("Svar: " siv);
+        erna = siv - jens;
+        System.out.printn(erna);
+    }
+}
---
+++ trix/1.22/Lokker.java
@@ -0,0 +1,13 @@
+
+class Lokker {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        while (x < 11) {
+            System.out.println(x);
+            x ++;
+        }
+    }
+}
